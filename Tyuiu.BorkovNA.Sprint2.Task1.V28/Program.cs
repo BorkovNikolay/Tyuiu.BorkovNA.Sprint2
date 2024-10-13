00e909e500b450a48fa295d6c6f83a5d72b7072e
Nikolay Borkov <ghost0dragon13@gmail.com>
2024-10-13 15:40:10 +0500

    task1

---
+++ Tyuiu.BorkovNA.Sprint2.Task1.V28/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Tyuiu.BorkovNA.Sprint2.Task1.V28.Lib;
+
+
+
+
+namespace Tyuiu.BorkovNA.Sprint2.Task1.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Cпринт #2 | Выполнил: Борков Н. А. | пктб-24-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                *");
+            Console.WriteLine("* Тема: Операции сравнения                                                 *");
+            Console.WriteLine("* Задание #1                                                               *");
+            Console.WriteLine("* Вариант #28                                                              *");
+            Console.WriteLine("* Выполнил: Борков Н. А. | пктб-24-1                                       *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,          *");
+            Console.WriteLine("* последовательность операций не должна нарушаться) и арифметических       *");
+            Console.WriteLine("* выражений, которая вернет логическую последовательность(массив):         *");
+            Console.WriteLine("* (True, False, True, False, True, False), при x = 233, y = 122            *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* x = 233, y = 122                                                         *");
+            int a = 247;
+            int b = 654;
+            int c = 671;
+            int d = 671;
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            Console.WriteLine("Результат = ");
+            for (int i = 0; i < res.Length; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+
+}
