77c06baf5cb54fc7b35b46be07198a9da47bd57d
Nikolay Borkov <ghost0dragon13@gmail.com>
2024-10-13 14:59:41 +0500

    task1

--- Tyuiu.BorkovNA.Sprint2.Task0.V1.Lib/DataService.cs
+++ Tyuiu.BorkovNA.Sprint2.Task0.V1.Lib/DataService.cs
@@ -4,13 +4,13 @@ namespace Tyuiu.BorkovNA.Sprint2.Task0.V1.Lib
 {
     public class DataService : ISprint2Task0V1
     {
-       
+
         public bool[] GetCompareOperations(int x, int y)
         {
             bool[] res = new bool[6];
-            res[0] = x-111 == y;
-            res[1] = x-111 != y;
-            res[2] = y<x;
+            res[0] = x - 111 == y;
+            res[1] = x - 111 != y;
+            res[2] = y < x;
             res[3] = y > x;
             res[4] = y <= x;
             res[5] = y >= x;
--- Tyuiu.BorkovNA.Sprint2.Task0.V1.Test/DataServiceTest.cs
+++ Tyuiu.BorkovNA.Sprint2.Task0.V1.Test/DataServiceTest.cs
@@ -19,3 +19,4 @@ namespace Tyuiu.BorkovNA.Sprint2.Task0.V1.Test
         }
     }
 }
+
