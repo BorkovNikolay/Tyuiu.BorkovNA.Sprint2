0ded0cbf8714cbe5827782f4c32900f96855c7c7
Nikolay Borkov <ghost0dragon13@gmail.com>
2024-10-13 14:32:46 +0500

    Add project files.

---
+++ Tyuiu.BorkovNA.Sprint2.Task0.V1.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.BorkovNA.Sprint2.Task0.V1.Lib
+{
+    public class DataService : ISprint2Task0V1
+    {
+       
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+            res[0] = x-111 == y;
+            res[1] = x-111 != y;
+            res[2] = y<x;
+            res[3] = y > x;
+            res[4] = y <= x;
+            res[5] = y >= x;
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.BorkovNA.Sprint2.Task0.V1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Tyuiu.BorkovNA.Sprint2.Task0.V1.Lib;
+
+
+
+
+namespace Tyuiu.BorkovNA.Sprint2.Task0.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Cпринт #2 | Выполнил: Борков Н. А. | пктб-24-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                *");
+            Console.WriteLine("* Тема: Операции сравнения                                                 *");
+            Console.WriteLine("* Задание #0                                                               *");
+            Console.WriteLine("* Вариант #1                                                               *");
+            Console.WriteLine("* Выполнил: Борков Н. А. | пктб-24-1                                       *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,          *");
+            Console.WriteLine("* последовательность операций не должна нарушаться) и арифметических       *"); 
+            Console.WriteLine("* выражений, которая вернет логическую последовательность(массив):         *");
+            Console.WriteLine("* (True, False, True, False, True, False), при x = 233, y = 122            *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* x = 233, y = 122                                                         *");
+            int x = 233;
+            int y = 122;
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            Console.WriteLine("Результат = ");
+            for (int i = 0; i < res.Length; i++)
+            {
+                Console.WriteLine(res[i]);
+
+            }            
+            Console.ReadKey();
+
+        }
+    }
+
+}
