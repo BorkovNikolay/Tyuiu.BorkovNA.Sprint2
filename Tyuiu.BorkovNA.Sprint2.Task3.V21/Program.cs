462a50691e2c2033f3a2fab1e64807dfc1285dcf
Nikolay Borkov <ghost0dragon13@gmail.com>
2024-10-13 17:31:49 +0500

    task3

---
+++ Tyuiu.BorkovNA.Sprint2.Task3.V21/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Tyuiu.BorkovNA.Sprint2.Task3.V21.Lib;
+
+
+
+
+namespace Tyuiu.BorkovNA.Sprint2.Task3.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Cпринт #2 | Выполнил: Борков Н. А. | пктб-24-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                *");
+            Console.WriteLine("* Тема: Операции сравнения                                                 *");
+            Console.WriteLine("* Задание #3                                                               *");
+            Console.WriteLine("* Вариант #21                                                              *");
+            Console.WriteLine("* Выполнил: Борков Н. А. | пктб-24-1                                       *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Введите x =                                                              *");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            double res = ds.Calculate(x);
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+
+}
