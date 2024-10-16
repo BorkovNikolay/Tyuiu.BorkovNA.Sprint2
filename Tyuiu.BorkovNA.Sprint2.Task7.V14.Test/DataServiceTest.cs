68f0f508bdd661eada0a22f4870fe7c6f92e6a2d
Nikolay Borkov <ghost0dragon13@gmail.com>
2024-10-16 13:06:38 +0500

    r

---
+++ Tyuiu.BorkovNA.Sprint2.Task7.V14.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+using Tyuiu.BorkovNA.Sprint2.Task7.V14.Lib;
+
+namespace Tyuiu.BorkovNA.Sprint2.Task7.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
---
+++ Tyuiu.BorkovNA.Sprint2.Task7.V14/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Tyuiu.BorkovNA.Sprint2.Task7.V14.Lib;
+
+
+
+
+namespace Tyuiu.BorkovNA.Sprint2.Task7.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Cпринт #2 | Выполнил: Борков Н. А. | пктб-24-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                *");
+            Console.WriteLine("* Тема: Операции сравнения                                                 *");
+            Console.WriteLine("* Задание #7                                                               *");
+            Console.WriteLine("* Вариант #14                                                              *");
+            Console.WriteLine("* Выполнил: Борков Н. А. | пктб-24-1                                       *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Условие:вычислить, находится ли точка с координатами XY                  *");
+            Console.WriteLine("* в заштрихованной области.                                                *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Введите x =                                                              *");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("* Введите y =                                                              *");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            bool res = ds.CheckDotInShadedArea(x,y);
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+
+}
